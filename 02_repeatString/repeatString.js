1d75eaba666d3fb7e07232a47452020567c061ff
Grumpy-Tomato <cormier.tanner@gmail.com>
2023-11-25 12:51:07 -0400

    added: - code to pass the tests in exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let newString = "";
+        for(let i = num; i > 0; i--) {
+            newString = newString + string;
+        }
+        return newString;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
