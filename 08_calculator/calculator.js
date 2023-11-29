d12d44452510032b9de5d117e666ed931bc96566
Grumpy-Tomato <cormier.tanner@gmail.com>
2023-11-29 18:17:56 -0400

    - passed all calculator.js tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	array.forEach(item => {
+    sum += item;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = 1;
+  array.forEach(item => {
+    sum *= item;
+  })
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let sum = 1;
+  for(let i = 1; i <= number ; i++) {
+    sum = sum * i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
