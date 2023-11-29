836cc26b342835b1e97679d977aa990dbd4e7bb1
Grumpy-Tomato <cormier.tanner@gmail.com>
2023-11-29 19:42:00 -0400

    - passed tests for palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = stringToCheck => {
+    let newStringToCheck = stringToCheck.toLowerCase().replace(/[\s\.,?!]/g, "");
+    return newStringToCheck.split("").reverse().join("") === newStringToCheck;
 };
 
 // Do not edit below this line
